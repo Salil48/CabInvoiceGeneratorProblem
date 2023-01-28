9fc93d887f39ae7ceedd4522e195465d1313ea83
“Salil48” <salilchharia@gmail.com>
2023-01-28 21:00:30 +0530

    Calculate Fare

---
+++ Ride.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGenerator
+{
+    public class Ride
+    {
+        // variables used
+        public double distance;
+        public double time;
+
+        public Ride(double distance, double time)
+        {
+            this.distance = distance;
+            this.time = time;
+        }
+    }
+}
